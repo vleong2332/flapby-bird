34b7a65ea6ba6bb4be683598aba1ef034f4db517
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-04-15 23:17:54 -0500

    A>gulpfile.js with default and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,63 @@
-var gulp   = require('gulp'),
-	uglify = require('gulp-uglify');
+var gulp       = require('gulp'),
+	uglify     = require('gulp-uglify'),
+	reload     = require('gulp-livereload'),
+	jshint     = require('gulp-jshint'),
+	sass       = require('gulp-sass'),
+	imagemin   = require('gulp-imagemin'),
+	bower      = require('gulp-bower'),
+	concat     = require('gulp-concat'),
+	connect    = require('gulp-connect'),
+	minifyHTML = require('gulp-minify-html'),
+	rename     = require('gulp-rename'),
+	bourbon    = require('node-bourbon'),
+	neat       = require('node-neat'),
+	buffer     = require('vinyl-buffer'),
+	source     = require('vinyl-source-stream'),
+	browserify = require('browserify'),
+	uglifycss  = require('gulp-uglifycss');
 
-gulp.task('default', function() {
-	// Place code for your default task here
-	gulp.src('site/js/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('site/minjs'));
+
+gulp.task('default', ['jshint', 'sass', 'watch']);
+
+/* Development Tasks */
+gulp.task('jshint', function() {
+	return gulp.src('site/js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+gulp.task('sass', function() {
+	return gulp.src('site/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('site/css'));
+});
+gulp.task('watch', function() {
+	gulp.watch('site/js/*.js', ['jshint']);
+	gulp.watch('site/scss/*.scss', ['sass']);
+});
+
+/* Deployment Tasks */
+gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
+gulp.task('html', function() {
+	gulp.src('site/*.html')
+		.pipe(minifyHTML())
+		.pipe(gulp.dest('build/'));
+});
+gulp.task('scripts', function() {
+	return browserify('./site/js/main.js')
+		.bundle()
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(uglify())
+		.pipe(gulp.dest('build/js'));
+});
+gulp.task('styles', function() {
+	gulp.src('site/css/*.css')
+		.pipe(concat('styles.css'))
+		.pipe(uglifycss())
+		.pipe(gulp.dest('build/css'));
+});
+gulp.task('images', function() {
+	gulp.src('site/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/img'));
 });
